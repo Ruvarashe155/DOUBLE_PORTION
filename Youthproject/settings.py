f425222bbf9a77b147ffddbc7e7027ec5d1dce1f
Ruvarashe Shoko <ruvarashe@Kali.lan>
2025-09-18 19:09:46 +0200

    Update ALLOWED_HOSTS to include Render's external hostname

--- Youthproject/settings.py
+++ Youthproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ps(d#g70m4_4k^-+=+s$_2t@-owam*g6pf$0hafs1zpmklvj_)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.208', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
