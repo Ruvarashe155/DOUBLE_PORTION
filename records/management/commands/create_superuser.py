0824f3bed6d4dccb9db1daaf9ae8201c1763874e
Ruvarashe Shoko <ruvarashe@Kali.lan>
2025-09-19 15:09:56 +0200

    Add makemigrations to deployment process

--- records/management/commands/create_superuser.py
+++ records/management/commands/create_superuser.py
@@ -1,17 +1,18 @@
-# records/management/commands/create_superuser.py
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from django.core.management import call_command
 
 class Command(BaseCommand):
-    help = 'Create a superuser automatically'
+    help = 'Creates a superuser with default credentials'
 
     def handle(self, *args, **kwargs):
-        if not User.objects.filter(username='admin').exists():
+        # Check if the superuser already exists
+        if not User.objects.filter(username="admin").exists():
             User.objects.create_superuser(
-                username='admin',
-                email='admin@example.com',
-                password='doubleportion'  # Change this to your desired password
+                username="admin",
+                email="admin@example.com",
+                password="adminpassword123"
             )
-            self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
+            self.stdout.write(self.style.SUCCESS('Superuser created successfully!'))
         else:
-            self.stdout.write(self.style.SUCCESS('Superuser already exists'))
+            self.stdout.write(self.style.SUCCESS('Superuser already exists!'))
